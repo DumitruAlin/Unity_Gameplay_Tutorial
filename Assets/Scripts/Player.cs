195f32df54503e3d7b62b12bf988cc27a642a686
DumitruAlin <simoiu.alin@gmail.com>
2018-01-13 16:27:23 +0200

    + added PlayerController and Player scripts, can move objects that have the Player script attached

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+// RequireComponent will be automatically added ! :D sweet
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+
+	public float MoveSpeed = (float) 7.5;
+	private PlayerController _controller;
+	
+	void Start ()
+	{
+		// attached to same object as this script
+		_controller = GetComponent<PlayerController>();
+	}
+	
+	void Update () {
+		Vector3 moveInput 		= new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		Vector3 moveVelocity 	= moveInput.normalized * MoveSpeed;
+		
+		_controller.Move(moveVelocity);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+	Vector3 _velocity;
+	Rigidbody _myRigidBody;
+	
+	void Start ()
+	{
+		_myRigidBody = GetComponent<Rigidbody>();
+	}
+
+	public void Move(Vector3 inVelocity)
+	{
+		_velocity = inVelocity;
+	}
+	
+	// move rigid body only in the fixed update, at small regular increments (physics ? ? ? )
+	private void FixedUpdate()
+	{
+		_myRigidBody.MovePosition(_myRigidBody.position + _velocity * Time.fixedDeltaTime);
+	}
+}
