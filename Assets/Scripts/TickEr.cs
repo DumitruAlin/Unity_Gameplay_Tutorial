4d02f25b3557feab3de46b968e76bb580d88c02d
DumitruAlin <simoiu.alin@gmail.com>
2018-01-13 14:10:16 +0200

    * Add Project Assets Folder

---
+++ Assets/Scripts/TickEr.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class TickEr : MonoBehaviour {
+
+		// Use this for initialization
+		private void Start () {
+		
+		}
+	
+		// Update is called once per frame
+		private void Update () {
+		
+		}
+	}
+}
