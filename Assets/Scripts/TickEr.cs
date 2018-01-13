c58eb92cc81c5b1732bed4adb9650e40c1b1fc93
DumitruAlin <simoiu.alin@gmail.com>
2018-01-13 15:53:03 +0200

    * Blender axes on import R&D * Clean scene, no fancy lights

--- Assets/Scripts/TickEr.cs
+++ Assets/Scripts/TickEr.cs
@@ -4,15 +4,15 @@ public class TickEr : MonoBehaviour {
 
 	// Use this for initialization
 	// ReSharper disable once Unity.RedundantEventFunction
-	private void Start () {
-		
+	private void Start ()
+	{
+		Debug.Log("<color=blue>Object Initialised: </color>" + this);
 	}
 
 	private int _x;
 	// Update is called once per frame
 	private void Update ()
 	{
-		System.Console.Write("Tick-ing: ");
-		System.Console.WriteLine(_x++);
+		Debug.Log("<color=red>x: </color>" + _x++);		
 	}
 }
