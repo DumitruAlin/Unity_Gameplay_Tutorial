ad41aa6e21a308959c5df44a866714682eb32fd3
DumitruAlin <simoiu.alin@gmail.com>
2018-01-13 16:59:54 +0200

    + added Gucci Glasses * quick code cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,33 @@
 public class Player : MonoBehaviour
 {
 
-	public float MoveSpeed = (float) 7.5;
-	private PlayerController _controller;
-	
-	void Start ()
+	public 	float 				MoveSpeed = (float) 7.5;
+	private Camera 				_viewCamera;
+	private PlayerController 	_controller;
+
+	private void Start ()
 	{
 		// attached to same object as this script
 		_controller = GetComponent<PlayerController>();
+		
+		_viewCamera = Camera.main;
 	}
-	
-	void Update () {
-		Vector3 moveInput 		= new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+	private void Update () {
+		Vector3 moveInput 		= new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 		Vector3 moveVelocity 	= moveInput.normalized * MoveSpeed;
 		
 		_controller.Move(moveVelocity);
+
+		Ray ray = _viewCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+		float rayDistance;
+
+		if (groundPlane.Raycast(ray, out rayDistance))
+		{
+			Vector3 point = ray.GetPoint(rayDistance);
+			//Debug.DrawLine(ray.origin, point, Color.red);
+			_controller.LookAt(point);
+		}
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,10 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-	Vector3 _velocity;
-	Rigidbody _myRigidBody;
-	
-	void Start ()
+	private Vector3 	_velocity;
+	private Rigidbody 	_myRigidBody;
+
+	private void Start ()
 	{
 		_myRigidBody = GetComponent<Rigidbody>();
 	}
@@ -15,8 +15,14 @@ public class PlayerController : MonoBehaviour
 	{
 		_velocity = inVelocity;
 	}
+
+	public void LookAt(Vector3 lookPoint)
+	{
+		var heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+		transform.LookAt(heightCorrectedPoint);
+	}
 	
-	// move rigid body only in the fixed update, at small regular increments (physics ? ? ? )
+	// move rigid body only in the fixed update, at small regular increments (for physics ?)
 	private void FixedUpdate()
 	{
 		_myRigidBody.MovePosition(_myRigidBody.position + _velocity * Time.fixedDeltaTime);
